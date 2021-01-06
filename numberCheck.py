b6458ccbec722cdd3a3c087af00570be895dc150
Anshuman Kumar Sharma <35607867+ans-sharma@users.noreply.github.com>
2021-01-06 23:29:24 +0530

    small fix, removed commented code, modify check

--- numberCheck.py
+++ numberCheck.py
@@ -2,26 +2,18 @@
 value = input("Enter the value: ")
 flag = False
 for i in range(0, len(value)):
-    # print(ord(char))
     if value[i] == "-":
         if i == len(value)-1:
-            print("Not a number")
+            flag = True
+            print("Not a Number")
             break
         elif ord(value[i+1]) < 48 or ord(value[i+1]) > 57:
-            print("Not a number")
+            flag = True
+            print("Not a Number")
             break
-    # if flag == True:
-    #     if i == len(value)-1:
-    #         print("Not a number")
-    #         break
-    #     elif ord(value[i+1]) < 48 or ord(value[i+1]) > 57:
-    #         print("Not a number")
-    #         break
-    if ord(value[i]) < 48 or ord(value[i]) > 57:
-        print("Not a number")
-        flag == True
+    elif ord(value[i]) < 48 or ord(value[i]) > 57:
+        print("Not a Number")
+        flag = True
         break
-if flag == True:
+if flag == False:
     print("Its a Number")
-# if(flag == False):
-#     print("not number")
