2707ddd960e5306909c0ea743afb2cab89319000
Anshuman Kumar Sharma <35607867+ans-sharma@users.noreply.github.com>
2021-01-07 20:01:47 +0530

    function added

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -1,12 +1,16 @@
+def pascalTriangle(rows):
+    print("Pascal's Triangle")
+    for i in range(0, rows):
+        for space in range(1, (rows+1)-i):
+            print(" ", end="")
+        for j in range(0, i+1):
+            if j == 0 or i == 0:
+                one = 1
+            else:
+                one = int(one * (i-j+1)/j)
+            print(" " + str(one), end="")
+        print("")
+
+
 rows = int(input("Enter the no. of rows: "))
-print("Pascal's Triangle")
-for i in range(0, rows):
-    for space in range(1, (rows+1)-i):
-        print(" ", end="")
-    for j in range(0, i+1):
-        if j == 0 or i == 0:
-            one = 1
-        else:
-            one = int(one * (i-j+1)/j)
-        print(" " + str(one), end="")
-    print("")
+pascalTriangle(rows)
--- PythagoreanTriples.py
+++ PythagoreanTriples.py
@@ -1,7 +1,11 @@
+def pythagoreanTriples(upperBound):
+    for a in range(1, upperBound+1):
+        for b in range(a, upperBound+1):
+            pow_cal = pow(a, 2) + pow(b, 2)
+            num = pow(pow_cal, 0.5)
+            if num % 1 == 0:
+                print("(" + str(a) + "," + str(b) + "," + str(int(num)) + ")")
+
+
 upperBound = int(input("Enter the Upper Limit: "))
-for a in range(1, upperBound+1):
-    for b in range(a, upperBound+1):
-        pow_cal = pow(a, 2) + pow(b, 2)
-        num = pow(pow_cal, 0.5)
-        if num % 1 == 0:
-            print("(" + str(a) + "," + str(b) + "," + str(int(num)) + ")")
+pythagoreanTriples(upperBound)
